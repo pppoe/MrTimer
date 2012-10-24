ba1a9e866e56777ccd4b12b9f0e750425b3dcfe0
mr.pppoe <mr.pppoe@gmail.com>
2012-10-23 23:03:00 -0400

    Initial Commit

---
+++ MrTimer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MrTimer
+//
+//  Created by li haoxiang on 10/23/12.
+//  Copyright (c) 2012 Haoxiang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MrTimer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MrTimer
+//
+//  Created by li haoxiang on 10/23/12.
+//  Copyright (c) 2012 Haoxiang Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
